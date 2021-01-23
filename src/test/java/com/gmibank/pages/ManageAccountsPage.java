246bc309b79dab86f0a94992fa49f14a43759d1c
tugba54-test <akcatugba@yahoo.com>
2021-01-23 01:07:19 -0700

    Third commit

--- src/test/java/com/gmibank/pages/ManageAccountsPage.java
+++ src/test/java/com/gmibank/pages/ManageAccountsPage.java
@@ -1,11 +1,23 @@
 package com.gmibank.pages;
 
+import com.gmibank.utilities.BrowserUtils;
 import com.gmibank.utilities.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ManageAccountsPage {
+
+  BrowserUtils BUtils=new BrowserUtils();
+  String[] ar={"CHECKING","SAVING","CREDIT_CARD","INVESTING"};
+  ArrayList<String>type=new ArrayList<String>();
 
     public void ManageAccountsPage(){
 
@@ -29,10 +41,43 @@ public class ManageAccountsPage {
     @FindBy(id="tp-account-employee")
     public WebElement employee;
 
-    @FindBy(id="save-entity")
+    @FindBy(xpath="//button[@id='save-entity']")
     public WebElement saveBtn;
 
     @FindBy(xpath="//div[@class='invalid-feedback']")
     public WebElement errorMessage;
+
+    public void validateError(String message){
+//    BUtils.waitForPageToLoad(30);//bug
+//
+//      BUtils.scrollDown(50);
+//      BUtils.waitForClickablility(saveBtn,30);
+//      saveBtn.click();
+//      BUtils.scrollUp(100);
+//      Assert.assertEquals("mismatch message",errorMessage.getText(),message);
+    }
+
+    public void ValidateTypeOfAccount(){
+      AccountType.click();
+      Select select=new Select(AccountType);
+      List<WebElement> Alloption=select.getOptions();
+      String op;
+
+      type.add("CHECKING");
+      type.add("SAVING");
+      type.add("CREDIT_CARD");
+      type.add("INVESTING");
+
+      Iterator<WebElement>it=Alloption.iterator();
+
+      while(it.hasNext()) {
+        String text=it.next().getText();
+        Assert.assertEquals("mismatch Type of Account",text,type);
+        System.out.println("Actual "+text+" Expected "+type);
+      }
+
+
+
+    }
 
 }
--- src/test/java/com/gmibank/stepDefinitions/Us13AddingAccountSteps.java
+++ src/test/java/com/gmibank/stepDefinitions/Us13AddingAccountSteps.java
@@ -2,6 +2,7 @@ package com.gmibank.stepDefinitions;
 
 import com.gmibank.pages.CommonWebElements;
 import com.gmibank.pages.LoginPage;
+import com.gmibank.pages.ManageAccountsPage;
 import com.gmibank.utilities.Driver;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -10,6 +11,8 @@ public class Us13AddingAccountSteps {
 
     CommonWebElements cmn=new CommonWebElements();
     LoginPage login=new LoginPage();
+    ManageAccountsPage mangeAc=new ManageAccountsPage();
+
 
     @When("And user provides username  {string} and password {string}")
     public void and_user_provides_username_and_password(String username, String password) {
@@ -29,23 +32,24 @@ public class Us13AddingAccountSteps {
     }
 
     @Then("description can not be blank  Employee see that message{string}")
-    public void description_can_not_be_blank_Employee_see_that_message(String string) {
+    public void description_can_not_be_blank_Employee_see_that_message(String message) {
+        mangeAc.validateError(message);
 
     }
 
     @Then("employee should create a description")
     public void employee_should_create_a_description() {
-
+      mangeAc.description.sendKeys("Added description to employee new account");
     }
 
     @Then("employee provide a balance as a Dollar")
     public void employee_provide_a_balance_as_a_Dollar() {
-
+        mangeAc.balance.sendKeys("1");
     }
 
     @When("Employee click account type can see option  CHECKING,SAVING,CREDIT_CARD or INVESTING")
     public void employee_click_account_type_can_see_option_CHECKING_SAVING_CREDIT_CARD_or_INVESTING() {
-
+        mangeAc.ValidateTypeOfAccount();
     }
 
     @Then("Account status should be defined as ACTIVE, SUSPENDED or CLOSED")
