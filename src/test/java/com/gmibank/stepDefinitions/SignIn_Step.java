604ee158c0effd18c54634c508cc1e7890230219
Gul JAVA <gulgultekin1@hotmail.com>
2021-01-26 06:41:29 -0500

    US-5 new code

--- src/test/java/com/gmibank/stepDefinitions/SignIn_Step.java
+++ src/test/java/com/gmibank/stepDefinitions/SignIn_Step.java
@@ -17,7 +17,7 @@ public class SignIn_Step {
     JavascriptExecutor executor = (JavascriptExecutor) (Driver.getDriver());
 
 
-
+   //user provide invalid username , valid password
     @Given("user provides invalid username {string} and valid password {string}and click")
     public void user_provides_invalid_username_and_valid_password_and_click(String username, String password) {
 
@@ -27,6 +27,7 @@ public class SignIn_Step {
         Driver.wait(2);
     }
 
+    //error message is displayed
     @Then("user should see error message")
     public void user_should_see_error_message() {
 
@@ -34,6 +35,7 @@ public class SignIn_Step {
         Assert.assertTrue(loginPage.failedToSignInMessage.isDisplayed());
     }
 
+    //user provide valid username , invalid password and error message is displayed
     @Given("user provides valid {string} and invalid {string}and click")
     public void user_provides_valid_and_invalid_and_click(String username, String password) {
 
@@ -45,6 +47,7 @@ public class SignIn_Step {
         Assert.assertTrue(loginPage.failedToSignInMessage.isDisplayed());
     }
 
+    //user provide invalid username , invalid password and error message is displayed
     @Given("user provides invalid {string} and invalid {string}and click")
     public void user_provides_invalid_and_invalid_and_click(String username, String password) {
 
@@ -55,6 +58,7 @@ public class SignIn_Step {
         Assert.assertTrue(loginPage.failedToSignInMessage.isDisplayed());
     }
 
+    // click on Did you forget your password? link
     @Then("Did you forget your password? link should be visible and clickable")
     public void did_you_forget_your_password_link_should_be_visible_and_clickable() {
 
@@ -62,14 +66,15 @@ public class SignIn_Step {
         Assert.assertTrue(loginPage.didYouForgetYourPasswordLink.isDisplayed());
     }
 
+    //Navigate to the reset your password page
     @Then("when user click link should navigate to the reset your password page")
     public void when_user_click_link_should_navigate_to_the_reset_your_password_page() {
 
-        //loginPage.didYouForgetYourPasswordLink.click();
         executor.executeScript("arguments[0].click();", loginPage.didYouForgetYourPasswordLink);
         //Driver.waitForVisibility(resetYourPasswordPage.emailInputBox,2);
     }
 
+    //
     @Then("user provide email  and click reset password button and see success message")
     public void user_provide_email_and_click_reset_password_button_and_see_success_message() {
 
